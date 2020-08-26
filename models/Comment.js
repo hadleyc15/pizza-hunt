5f49cfe6d5c89546b043bd8844ebad0de5500a2d
Chris Hadley <hadleyc15@yahoo.com>
2020-08-25 19:41:29 -0600

    added validation to user input

--- models/Comment.js
+++ models/Comment.js
@@ -4,16 +4,19 @@ const moment = require('moment');
 
 const ReplySchema = new Schema(
     {
-      // set custom id to avoid confusion with parent comment's _id field
+      // set custom id to avoid confusion with parent comment _id
       replyId: {
         type: Schema.Types.ObjectId,
         default: () => new Types.ObjectId()
       },
       replyBody: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
       },
       writtenBy: {
-        type: String
+        type: String,
+        required: true,
       },
       createdAt: {
         type: Date,
@@ -31,10 +34,13 @@ const ReplySchema = new Schema(
   const CommentSchema = new Schema(
     {
       writtenBy: {
-        type: String
+        type: String,
+        required: true
       },
       commentBody: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
       },
       createdAt: {
         type: Date,
